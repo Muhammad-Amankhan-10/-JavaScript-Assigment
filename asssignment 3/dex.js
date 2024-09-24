73164ae3f3d847b967c7527001d8b134e6b36b46
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2024-09-24 12:32:55 +0500

    Add files via upload

---
+++ asssignment 3/dex.js
@@ -0,0 +1,22 @@
+
+var a=Number(prompt("Enter time in 24-hour format (e.g., 1900 for 7 PM):"));
+
+if(a>=0 && a<1200 ){
+    alert("GOOD MORNING")
+}
+else if(a>=1200 && a<1700 ){
+    alert("GOOD AFETRNOON")
+}
+else if(a>=1700 && a<2100 ){
+    alert("GOOD EVENING")
+}
+else if(a>=2100 && a<2359 ){
+    alert("GOOD NIGHT")
+}
+else if(a>=0 && a<=1200 ){
+    alert("GOOD MORNING")
+}
+else alert("ERROR")
+
+
+
