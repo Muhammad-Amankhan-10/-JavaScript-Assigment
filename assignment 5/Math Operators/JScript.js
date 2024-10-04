1ad0a4e79bff7d79f9554d69aad386bc260b70cd
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2024-10-04 12:34:23 +0500

    Add files via upload

---
+++ assignment 5/Math Operators/JScript.js
@@ -0,0 +1,70 @@
+
+
+// chap # 3 Variables for Numbers //
+
+//  var a = 10;
+//  let c = a * 10;
+//  console.log(c)
+
+// var  grams = 100;
+// grams = grams + 25;
+// console.log(grams + 25);
+
+    // chap # 5  Math expressions: Familiar operators //
+
+//  var a = 20;
+//  var b = 20;
+//  console.log(a + b); 
+
+ //   var packs = 10;
+ //   var another_packet = 1;
+ //   var total = packs + another_packet;
+ //   console.log(total);
+
+//   var num = 10;
+//   var number2 = 10;
+//   var total_numbers = num * number2;
+//   console.log(total_numbers);
+
+ //  var a = 20;
+ //  var b = 20;
+ //  console.log(a / b); 
+
+    
+    // chap # 6  Math expressions: Unfamiliar operators //
+ 
+//  var num    = 1;
+//  var $price1 = num--;       <---*same value 
+//  console.log($price1); 
+    
+ //  var num     = 30;
+ //  var $price2 = --num;     <---*decrement of one    
+ //  console.log($price2);
+     
+//  var num     = 30;
+//  var $price3 = ++num;     <---*incrment of one 
+//  console.log($price3);
+
+ //   var num     = 10;
+ //   var $price4 = num++;    <---*same value   
+ //   console.log($price4);   
+
+//  var num     = 5;
+//  var $price5 = +-num;     <---* minus value 
+//  console.log($price5);
+
+
+    // chap # 7  Math expressions: Eliminating ambiguity //
+
+ // var a = 10;
+ // var b = 10; 
+ // console.log(a + b * 20)
+
+//  var totalCost = (2 * 10) + 10 
+//  console.log(totalCost) 
+
+ //  var totalCost = (2 * 10) + (2 * 10) 
+ //  console.log(totalCost) 
+
+//  var totalCost = (2 * 10) + (2 * 10) / 20
+//  console.log(totalCost)
