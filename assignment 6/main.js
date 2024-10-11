1f02a9990f7f7a2dad5154301302b2732d21cbc7
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2024-10-11 13:23:48 +0500

    Add files via upload

---
+++ assignment 6/main.js
@@ -0,0 +1,23 @@
+   
+//  JAVASCRIPT PRACTICE  //
+
+//   Nested Loop 
+  
+// var firstName = ["Apple","Mango","Banana"];
+// var lastName  = ["Juice","Icecream","Shake"];      // We also makes multiple VAR 
+// for (var i = 0; i < firstName.length; i++);
+// for (var j = 0; j < lastName.length; j++);
+// console.log(firstName[i] + lastName[j]);          
+
+//  //   For Loop 
+ 
+
+// var names = ["Aman","Khan","shb"];
+// for(var i = 0; i < names.lenght; i++);
+//  console.log(names[i]+ " " +i);       // We can use , + and space etc in Double Qutation.    
+
+ 
+// for(var i = 0; i < 10; i++);
+//     console.log(i);
+ 
+    
