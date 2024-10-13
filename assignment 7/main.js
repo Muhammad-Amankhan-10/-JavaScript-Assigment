e085c87a9eacae1f290174dd972714454cf0a758
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2024-10-13 13:48:32 +0500

    Add files via upload

---
+++ assignment 7/main.js
@@ -0,0 +1,32 @@
+// var cityToCheck = "KaRaCHI";
+// var firstThreeChars = cityToCheck.slice(0, 3).toLowerCase();
+// var fourthChar = cityToCheck.slice(3, 4).toUpperCase();
+// var remainingChars = cityToCheck.slice(4).toLowerCase();
+// var modifiedCity = firstThreeChars + fourthChar + remainingChars;
+// console.log(modifiedCity);
+
+ 
+ 
+// var country = prompt("Enter a Country");
+// var charsIncountry = country.length;
+// if (charsIncountry > 3) {
+// countryAbbrev = country.slice(0, 3).toUpperCase();
+// console.log(countryAbbrev);
+//  }
+
+
+
+
+//  let str = "you are a bad boy and you good boy";
+
+
+//  let length = str.length;
+//  console.log(length);  // Output: 34
+
+
+//  let part = str.slice(0, 17);  
+//  console.log(part);  // Output: "you are a bad boy "
+
+
+// let part2 = str.substring(10 , 17);  
+//  console.log(part2);  // Output: "bad boy "
