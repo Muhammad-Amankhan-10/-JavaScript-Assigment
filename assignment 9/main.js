1597f61b7fecb14053d670ffaddf8635b7f08d93
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2024-10-27 13:52:45 +0500

    Add files via upload

---
+++ assignment 9/main.js
@@ -0,0 +1,60 @@
+   
+    // java script assignment # 9
+
+    // chap # 21  Changing case
+
+ // var cityToCheck = "KaRaCHI";
+ // var firstThreeChars = cityToCheck.slice(0, 3).toLowerCase();
+ // var fourthChar = cityToCheck.slice(3, 4).toUpperCase();
+ // var remainingChars = cityToCheck.slice(4).toLowerCase();
+ // var modifiedCity = firstThreeChars + fourthChar + remainingChars;
+ // console.log(modifiedCity);
+
+      
+    // chap # 22  Strings: Measuring length and extracting part
+  
+ //  var str = prompt("Enter any text");
+ //  var numberChars = str.length;
+
+ //  for(var i = 0; i < numberChars; i++){
+   
+ //     if(str.slice(i, i + 2) === " " ){
+ //     alert("No double space")
+ //     break;
+ //     }  
+
+ //  }
+
+
+    // chap # 23  Strings: Finding segments
+
+ //  var text = "world war II";
+ //  for ( var i = 0; i < text.lenght; i++ ){
+ //     if (text.slice(i, i + 12) === "world war II"){
+ //         text = text.slice(0, i) + "the second world war" + text.slice(i + 12);
+ //     }
+ // }
+ //   console.log(text);
+
+
+    // chap # 24  Strings:  Strings: Finding a character at a location
+
+ // var text = prompt("Enter your Name"); 
+ // for (var i = 0; i < text.length; i++) {
+ //    if (text.charAt(i) === " ") {
+ //     alert("Not Space allowed");
+ //      break;
+ //     }
+ //   }
+
+
+    // chap # 25 Strings: Replacing characters
+
+
+ // for (var i = 0; i < text.length; i++) {
+ //      if (text.slice(i, i + 12) === "World War II") {
+ //      text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+ //      }
+ //      }    
+ //      console.log();
+         
